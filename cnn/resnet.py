29e57424d5c1df6b267d358ff6c568853794195e
masanari kimura <noconoco.lib@gmail.com>
2018-03-13 13:37:40 +0900

    add resnet

--- cnn/resnet.py
+++ cnn/resnet.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 
 class BasicBlock(nn.Module):
@@ -55,3 +54,37 @@ class BottleNeck(nn.Module):
         h = F.relu(h)
 
         return h
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, n_blocks, n_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, n_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, n_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, n_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, n_blocks[3], stride=2)
+
+    def forward(self, x):
+        h = F.relu(self.bn1(self.conv1(x)))
+        h = self.layer1(h)
+        h = self.layer2(h)
+        h = self.layer3(h)
+        h = self.layer4(h)
+        h = F.avg_pool2d(h, 4)
+        h = h.view(h.size(0), -1)
+        h = self.linear(h)
+
+        return h
+
+    def _make_layer(self, block, planes, n_blocks, stride):
+        strides = [stride] + [1] + (n_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+
+        return nn.Sequential(*layers)
