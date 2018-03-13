05213601cc683524c57bb8940bf2cb5dde855059
masanari kimura <noconoco.lib@gmail.com>
2018-03-13 13:30:56 +0900

    add resnet

--- cnn/resnet.py
+++ cnn/resnet.py
@@ -22,7 +22,36 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         h = F.relu(self.bn1(self.conv1(x)))
-        h = F.relu(self.bn2(self.conv2(h)))
+        h = self.bn2(self.conv2(h))
+        h += self.shortcut(x)
+        h = F.relu(h)
 
         return h
 
+
+class BottleNeck(nn.Module):
+    expansion = 4
+    def __init__(self, in_planes, planes, stride=1):
+        super(BottleNeck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes),
+            )
+
+    def forward(self, x):
+        h = F.relu(self.bn1(self.conv1(x)))
+        h = F.relu(self.bn2(self.conv2(h)))
+        h = self.bn3(self.conv3(h))
+        h += self.shortcut(h)
+        h = F.relu(h)
+
+        return h
